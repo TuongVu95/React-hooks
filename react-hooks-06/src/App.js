00525915e1705c9f2814f6a07998cddf9859a421
Vũ <46469424+TuongVu95@users.noreply.github.com>
2022-02-14 11:15:17 +0700

    useMemo

---
+++ react-hooks-06/src/App.js
@@ -0,0 +1,27 @@
+import React,{useMemo, useState} from 'react';
+import TodoList from './components/TodoList';
+
+function exNumber(value) {
+  console.log(value)
+  return value * 2;
+}
+
+function App() {
+  const [state, setstate] = useState(0);
+  const [num, setNum] = useState(1);
+
+  const numbers = useMemo(() => {
+    return exNumber(num);
+  }, [num]);
+
+  return (
+    <div className="App">
+      <h1>{state}</h1>
+      <h2>{numbers}</h2>
+      <button onClick={() => {setstate(state + 1) }}>CLick</button>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
